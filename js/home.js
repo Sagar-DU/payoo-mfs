7ae7f7f1843d88f567d6243740484eede3cbce6f
Sagar-DU <nahidhasansagar@gmail.com>
2025-05-07 18:12:27 +0600

    comment added

--- js/home.js
+++ js/home.js
@@ -6,13 +6,18 @@ document.getElementById('btn-add-money').addEventListener('click', function () {
     const pinNumber = document.getElementById('pin-number').value;
     // Validate pin 
     if (pinNumber === '1234') {
+        // Get Balance 
         const balance = document.getElementById('balance-amount').innerText;
+        // Add the money with input amount make sure to change their type
         const newBalance = parseFloat(balance) + parseFloat(moneyAmount);
+        // Update balance 
         document.getElementById ('balance-amount').innerText = newBalance;
+        // Clear input fields 
         document.getElementById ('money-amount').value = '';
         document.getElementById ('pin-number').value = '';
     }
     else {
+        // Message incase of pin miss match 
         alert('Faild to add money. Try again!')
     }
 })
