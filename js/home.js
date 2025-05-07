224ad090a0d797c3b829e7b9b9870149682f9f9d
Sagar-DU <nahidhasansagar@gmail.com>
2025-05-07 15:24:30 +0600

    add money in progress

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-console.log ('home added')
+// Add Money to the account
+document.getElementById ('btn-add-money').addEventListener ('click', function(){
+    // Get The Money 
+    const moneyAmount = document.getElementById ('money-amount').value;
+    // Get Pin 
+    const pinNumber = document.getElementById ('pin-number').value;
+    // Validate pin 
+})
