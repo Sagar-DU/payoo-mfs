1ec5355667c7f5e21e828614cd7b04f5d9f7ec9a
Sagar-DU <nahidhasansagar@gmail.com>
2025-05-07 18:07:41 +0600

    add money section done

--- js/home.js
+++ js/home.js
@@ -1,8 +1,18 @@
 // Add Money to the account
-document.getElementById ('btn-add-money').addEventListener ('click', function(){
+document.getElementById('btn-add-money').addEventListener('click', function () {
     // Get The Money 
-    const moneyAmount = document.getElementById ('money-amount').value;
+    const moneyAmount = document.getElementById('money-amount').value;
     // Get Pin 
-    const pinNumber = document.getElementById ('pin-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
     // Validate pin 
+    if (pinNumber === '1234') {
+        const balance = document.getElementById('balance-amount').innerText;
+        const newBalance = parseFloat(balance) + parseFloat(moneyAmount);
+        document.getElementById ('balance-amount').innerText = newBalance;
+        document.getElementById ('money-amount').value = '';
+        document.getElementById ('pin-number').value = '';
+    }
+    else {
+        alert('Faild to add money. Try again!')
+    }
 })
