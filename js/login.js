712a3ed3a5515acb63b32790c0cb0fe3788d68c5
Sagar-DU <nahidhasansagar@gmail.com>
2025-05-07 14:11:16 +0600

    login page done

--- js/login.js
+++ js/login.js
@@ -1,4 +1,18 @@
 // Get the Element and add Event listner
-document.getElementById ('btn-login').addEventListener ('click',function(){
-    console.log ('clicked on  the button');
+document.getElementById('btn-login').addEventListener('click', function () {
+    // console.log ('clicked on  the button');
+    // Get the phone number 
+    const phoneNumber = document.getElementById('phone').value;
+    // Get the pin number 
+    const pinNumber = document.getElementById('pin').value;
+    // console.log(phoneNumber,pinNumber);
+    
+    // Validate in Fokira Way 
+    if (phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = '/home.html'
+    }
+    else {
+        alert ('Wrong Number or Pin')
+    }
+
 })
